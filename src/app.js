b65e0fe35e2431d965785a336a4674e93b4c57bf
James Rock <hello@jamesrock.me>
2026-03-12 15:14:03 +0000

    updates

--- src/app.js
+++ src/app.js
@@ -264,7 +264,6 @@ class Sequencer extends DisplayObject {
     this.node = makeNode('div', 'step-sequencer');
 
     this.steps = new Steps(this);
-    // this.channels = this.setupChannelStrips();
     this.sounds = new SoundManager({
       'kick': '/audio/kick.mp3',
       'snare': '/audio/snare.mp3',
@@ -532,42 +531,6 @@ class Sequencer extends DisplayObject {
   disable(beat) {
 
     this.instrument.steps[beat] = 0;
-
-  };
-  setupChannelStrips() {
-
-    const Kick = new Instrument('BassDrum');
-    const Snare = new Instrument('SnareDrum');
-    const RimShot = new Instrument('RimShot');
-    const Clap = new Instrument('handClaP');
-    const LowConga = new Instrument('LowConga');
-    const LowTom = new Instrument('LowTom');
-    const MidConga = new Instrument('MidConga');
-    const MidTom = new Instrument('MidTom');
-    const HiConga = new Instrument('HiConga');
-    const HiTom = new Instrument('HiTom');
-    const Accent = new Instrument('ACcent');
-    const Clave = new Instrument('CLave');
-    const Maracas = new Instrument('MAracas');
-    const CowBell = new Instrument('CowBell');
-    const Cymbal = new Instrument('CYmbal');
-    const OpenHats = new Instrument('OpenHats');
-    const ClosedHats = new Instrument('ClosedHats');
-
-    return [
-      new Channel([Accent], [new LevelKnob()]),
-      new Channel([Kick], [new LevelKnob(), new ToneKnob(), new DecayKnob()]),
-      new Channel([Snare], [new LevelKnob(), new ToneKnob(), new SnappyKnob()]),
-      new Channel([LowConga, LowTom], [new LevelKnob(), new TuningKnob()]),
-      new Channel([MidConga, MidTom], [new LevelKnob(), new TuningKnob()]),
-      new Channel([HiConga, HiTom], [new LevelKnob(), new TuningKnob()]),
-      new Channel([Clave, RimShot], [new LevelKnob()]),
-      new Channel([Maracas, Clap], [new LevelKnob()]),
-      new Channel([CowBell], [new LevelKnob()]),
-      new Channel([Cymbal], [new LevelKnob(), new ToneKnob(), new DecayKnob()]),
-      new Channel([OpenHats], [new LevelKnob(), new DecayKnob()]),
-      new Channel([ClosedHats], [new LevelKnob()])
-    ];
 
   };
   save() {
---
+++ src/arrangement.js
@@ -0,0 +1,36 @@
+export const setupChannelStrips = () => {
+
+  const Kick = new Instrument('BassDrum');
+  const Snare = new Instrument('SnareDrum');
+  const RimShot = new Instrument('RimShot');
+  const Clap = new Instrument('handClaP');
+  const LowConga = new Instrument('LowConga');
+  const LowTom = new Instrument('LowTom');
+  const MidConga = new Instrument('MidConga');
+  const MidTom = new Instrument('MidTom');
+  const HiConga = new Instrument('HiConga');
+  const HiTom = new Instrument('HiTom');
+  const Accent = new Instrument('ACcent');
+  const Clave = new Instrument('CLave');
+  const Maracas = new Instrument('MAracas');
+  const CowBell = new Instrument('CowBell');
+  const Cymbal = new Instrument('CYmbal');
+  const OpenHats = new Instrument('OpenHats');
+  const ClosedHats = new Instrument('ClosedHats');
+
+  return [
+    new Channel([Accent], [new LevelKnob()]),
+    new Channel([Kick], [new LevelKnob(), new ToneKnob(), new DecayKnob()]),
+    new Channel([Snare], [new LevelKnob(), new ToneKnob(), new SnappyKnob()]),
+    new Channel([LowConga, LowTom], [new LevelKnob(), new TuningKnob()]),
+    new Channel([MidConga, MidTom], [new LevelKnob(), new TuningKnob()]),
+    new Channel([HiConga, HiTom], [new LevelKnob(), new TuningKnob()]),
+    new Channel([Clave, RimShot], [new LevelKnob()]),
+    new Channel([Maracas, Clap], [new LevelKnob()]),
+    new Channel([CowBell], [new LevelKnob()]),
+    new Channel([Cymbal], [new LevelKnob(), new ToneKnob(), new DecayKnob()]),
+    new Channel([OpenHats], [new LevelKnob(), new DecayKnob()]),
+    new Channel([ClosedHats], [new LevelKnob()])
+  ];
+
+};
