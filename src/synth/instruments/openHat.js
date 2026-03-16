f75df6ece055f84c412aa98ca8a2a81d9be48e37
James Rock <hello@jamesrock.me>
2026-03-16 21:12:23 +0000

    removes synth files

--- src/synth/instruments/openHat.js
+++
@@ -1,9 +0,0 @@
-import HiHat from "./hiHat";
-import { equalPower } from "helpers";
-
-export const openHat = (audioCtx, destination, time, { level, decay }) => {
-  // parameters
-  const outputLevel = equalPower(level);
-  const decayValue = decay * 3.6 + 90;
-  return HiHat(audioCtx, destination, time, outputLevel, decayValue);
-};
